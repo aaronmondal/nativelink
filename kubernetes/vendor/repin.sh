9094e5558b4c00a841a3c2607340319b2a78da8a
Aaron Siddhartha Mondal <aaron@tracemachina.com>
2025-03-14 23:51:32 +0100

    Prepare development cluster for otel

--- kubernetes/vendor/repin.sh
+++ kubernetes/vendor/repin.sh
@@ -4,3 +4,13 @@
 wget \
     https://storage.googleapis.com/tekton-releases/operator/previous/v0.75.0/release.yaml \
     -O tekton-operator/tekton-operator.yaml
+
+# OpenTelemetry Operator
+wget \
+    https://github.com/open-telemetry/opentelemetry-operator/releases/download/v0.120.0/opentelemetry-operator.yaml \
+    -O opentelemetry-operator/opentelemetry-operator.yaml
+
+# Tempo Operator
+wget \
+    https://github.com/grafana/tempo-operator/releases/download/v0.15.3/tempo-operator.yaml \
+    -O tempo-operator/tempo-operator.yaml
