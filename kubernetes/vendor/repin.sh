5b02c336ee222f07d816f0561953321fa160ac20
Aaron Siddhartha Mondal <aaron@tracemachina.com>
2025-04-09 16:37:46 +0200

    Add Operator Lifecycle Manager

--- kubernetes/vendor/repin.sh
+++ kubernetes/vendor/repin.sh
@@ -4,3 +4,13 @@
 wget \
     https://storage.googleapis.com/tekton-releases/operator/previous/v0.75.0/release.yaml \
     -O tekton-operator/tekton-operator.yaml
+
+
+# Operator Lifecycle Manager
+wget \
+    https://github.com/operator-framework/operator-controller/releases/download/v1.2.0/operator-controller.yaml \
+    -O olm/controller.yaml
+
+wget \
+    https://github.com/operator-framework/operator-controller/releases/download/v1.2.0/default-catalogs.yaml \
+    -O olm-catalog/default-catalogs.yaml
